76a77f26e13f07bc05d856509f10b1db1eaa4d01
Evelyn Lacerda <evelyndslacerda@gmail.com>
2024-11-21 02:57:10 -0300

    feat: Create array of posts
    This commit is about create a posts array to show in json format. A
    constant has been created to get a post by id and show it through web
    address.

--- server.js
+++ server.js
@@ -1,10 +1,45 @@
 import express from 'express';
 
-const app = express()
+const posts = [
+    {
+        id: 1,
+        description: "This is the first post",
+        image: "https://placecat.com/louie/300/200",
+    },
+    {
+        id: 2,
+        description: "This is the second post",
+        image: "https://placecat.com/louie/300/200",
+    },
+    {
+        id: 3,
+        description: "This is the third post",
+        image: "https://placecat.com/louie/300/200",
+    },
+    {
+        id: 4,
+        description: "This is the fourth post",
+        image: "https://placecat.com/louie/300/200",
+    },
+    {
+        id: 5,
+        description: "This is the fifth post",
+        image: "https://placecat.com/louie/300/200",
+    }
+];
+
+function getPostById(id) {
+    return posts.findIndex((post) => post.id === Number(id));
+};
+
+const app = express();
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
 
-app.get("/api", (req, res) => {
-    res.status(200).send("Hello World");
+app.get("/posts/:id", (req, res) => {
+    const index = getPostById(req.params.id);
+    res.status(200).json(posts[index]);
 });
